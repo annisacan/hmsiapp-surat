05c5ce264c89ec4d9d79deacb32e937ef6fff66e
salsaabiila <89976076+salsaabiila@users.noreply.github.com>
2024-06-23 16:09:05 +0700

    pembaruan edit request

--- resources/views/divisi/reqsurat.blade.php
+++ resources/views/divisi/reqsurat.blade.php
@@ -36,7 +36,9 @@
                                 <td class="text-center">{{ $loop->iteration }}</td>
                                 <td class="text-center">{{ $request->updated_at->format('Y-m-d') }}</td>
                                 <td class="text-center">{{ $request->nama_surat }}</td>
-                                <td class="text-center"><span class="priority-badge {{ strtolower($request->priority) }}">{{ $request->priority }}</span></td>
+                                <td class="text-center">
+                                    <span class="priority-badge {{ strtolower(str_replace(' ', '-', $request->priority)) }}">{{ $request->priority }}</span>
+                                </td>
                                 <td class="text-center">{{ \Carbon\Carbon::parse($request->tanggal_request)->format('Y-m-d H:i') }}</td>
                                 <td class="text-center">{{ $request->status ?? 'Finished' }}</td> {{--diambil dari sekre--}}
                                 <td class="text-center">
@@ -48,6 +50,7 @@
                                         <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this item?');">Hapus</button>
                                     </form>
                                 </td>
+
 
 
                             </tr>
@@ -119,64 +122,64 @@
     </div>
 </div>
 
+<!-- Modal Edit -->
 @foreach($requests as $request)
-    <!-- Modal Edit -->
-    <div class="modal fade" id="editModal{{ $request->id }}" tabindex="-1" aria-labelledby="editModalLabel{{ $request->id }}" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="editModalLabel{{ $request->id }}">Edit Request Surat</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form action="{{ route('request-surat.update', $request->id) }}" method="POST">
-                        @csrf
-                        @method('PUT')
-                        <div class="form-row">
-                            <div class="form-group col-md-4">
-                                <label for="edit-nama-surat">Nama Surat</label>
-                                <input type="text" class="form-control" id="edit-nama-surat" name="nama_surat" value="{{ $request->nama_surat }}">
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label for="edit-priority">Prioritas</label>
-                                <select class="form-control" id="edit-priority" name="priority">
-                                    <option {{ $request->priority == 'Urgent' ? 'selected' : '' }}>Urgent</option>
-                                    <option {{ $request->priority == 'Soon' ? 'selected' : '' }}>Soon</option>
-                                    <option {{ $request->priority == 'Not Urgent' ? 'selected' : '' }}>Not Urgent</option>
-                                </select>
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label for="edit-tanggal-request">Waktu Pelaksanaan</label>
-                                <input type="datetime-local" class="form-control" id="edit-tanggal-request" name="tanggal_request" value="{{ \Carbon\Carbon::parse($request->tanggal_request)->format('Y-m-d\TH:i') }}">
-                            </div>
+<div class="modal fade" id="editModal{{ $request->id }}" tabindex="-1" aria-labelledby="editModalLabel{{ $request->id }}" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editModalLabel{{ $request->id }}">Edit Request Surat</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="{{ route('request-surat.update', $request->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class="form-row">
+                        <div class="form-group col-md-4">
+                            <label for="edit-nama-surat">Nama Surat</label>
+                            <input type="text" class="form-control" id="edit-nama-surat{{ $request->id }}" name="nama_surat" value="{{ $request->nama_surat }}">
                         </div>
-                        <div class="form-group">
-                            <label for="edit-deskripsi-surat">Deskripsi Surat</label>
-                            <textarea class="form-control no-resize" id="edit-deskripsi-surat" rows="5" name="deskripsi_surat">{{ $request->deskripsi_surat }}</textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="edit-tipe-surat">Tipe Surat</label>
-                            <select class="form-control" id="edit-tipe-surat" name="tipe_surat">
-                                <option {{ $request->tipe_surat == 'Surat Undangan' ? 'selected' : '' }}>Surat Undangan</option>
-                                <option {{ $request->tipe_surat == 'Surat Peminjaman' ? 'selected' : '' }}>Surat Peminjaman</option>
-                                <option {{ $request->tipe_surat == 'Surat Kunjungan' ? 'selected' : '' }}>Surat Kunjungan</option>
+                        <div class="form-group col-md-4">
+                            <label for="edit-priority">Prioritas</label>
+                            <select class="form-control" id="edit-priority{{ $request->id }}" name="priority">
+                                <option {{ $request->priority == 'Urgent' ? 'selected' : '' }}>Urgent</option>
+                                <option {{ $request->priority == 'Soon' ? 'selected' : '' }}>Soon</option>
+                                <option {{ $request->priority == 'Not Urgent' ? 'selected' : '' }}>Not Urgent</option>
                             </select>
                         </div>
-                        <div class="form-group">
-                            <label for="edit-penerima-surat">Penerima Surat</label>
-                            <input type="text" class="form-control" id="edit-penerima-surat" name="penerima_surat" value="{{ $request->penerima_surat }}">
+                        <div class="form-group col-md-4">
+                            <label for="edit-tanggal-request">Waktu Pelaksanaan</label>
+                            <input type="datetime-local" class="form-control" id="edit-tanggal-request{{ $request->id }}" name="tanggal_request" value="{{ \Carbon\Carbon::parse($request->tanggal_request)->format('Y-m-d\TH:i') }}">
                         </div>
-                        <div class="d-flex justify-content-end">
-                            <button type="button" class="btn btn-secondary mr-2" data-dismiss="modal">Cancel</button>
-                            <button type="submit" class="btn btn-primary">Edit Request</button>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="edit-deskripsi-surat">Deskripsi Surat</label>
+                        <textarea class="form-control no-resize" id="edit-deskripsi-surat{{ $request->id }}" rows="5" name="deskripsi_surat">{{ $request->deskripsi_surat }}</textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="edit-tipe-surat">Tipe Surat</label>
+                        <select class="form-control" id="edit-tipe-surat{{ $request->id }}" name="tipe_surat">
+                            <option {{ $request->tipe_surat == 'Surat Undangan' ? 'selected' : '' }}>Surat Undangan</option>
+                            <option {{ $request->tipe_surat == 'Surat Peminjaman' ? 'selected' : '' }}>Surat Peminjaman</option>
+                            <option {{ $request->tipe_surat == 'Surat Kunjungan' ? 'selected' : '' }}>Surat Kunjungan</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="edit-penerima-surat">Penerima Surat</label>
+                        <input type="text" class="form-control" id="edit-penerima-surat{{ $request->id }}" name="penerima_surat" value="{{ $request->penerima_surat }}">
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <button type="button" class="btn btn-secondary mr-2" data-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-primary">Edit Request</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 @endforeach
 
 
